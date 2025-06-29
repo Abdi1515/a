abc8cc1bc1b4a5ba718b860a5d8ba44bb4476395
blue bars of boredom <124185230+Abdi1515@users.noreply.github.com>
2025-06-28 21:41:54 -0300

    Update npcfual.lua

--- npcfual.lua
+++ npcfual.lua
@@ -22,58 +22,135 @@ local function sendRandomMessage()
     generalChannel:DisplaySystemMessage(randomMessage)
 end
 
-local CurrentPart = nil
-local MaxInc = 16
+local myHuman = script.Parent:WaitForChild("Humanoid")
+local myRoot = script.Parent:WaitForChild("Torso")
+local myHead = script.Parent:WaitForChild("Head")
+local myFace = myHead:WaitForChild("face")
 
-function onTouched(hit)
-	if hit.Parent == nil then
-		return
-	end
+local oldHealth = myHuman.Health
+local fleeing = false
+local fleeLength = 5
+local fleeCountdown = fleeLength
+local pathCount = 0 
+local dead = false
 
-	local humanoid = hit.Parent:findFirstChild("Humanoid")
+local clone = script.Parent:Clone()
 
-	if humanoid == nil then
-		CurrentPart = hit
+function getUnstuck()
+	myHuman:Move(Vector3.new(math.random(-1,1),0,math.random(-1,1)))
+	myHuman.Jump = true
+	wait(0.5)
+
+function pathToLocation(location)
+	local path = game:GetService("PathfindingService"):CreatePath()
+	path:ComputeAsync(myRoot.Position,location)
+	local waypoints = path:GetWaypoints()
+	pathCount = pathCount + 1
+
+	if path.Status == Enum.PathStatus.Success then
+		local currentPathCount = pathCount
+		for i, waypoint in ipairs(waypoints) do
+			if currentPathCount ~= pathCount then
+				return
+			end
+			if waypoint.Action == Enum.PathWaypointAction.Jump then
+				myHuman.Jump = true
+			end
+			myHuman:MoveTo(waypoint.Position)
+			delay(0.5, function()
+				if myHuman.WalkToPoint.Y > myRoot.Position.Y then
+					myHuman.Jump = true
+				end
+			end)
+			local moveSuccess = myHuman.MoveToFinished:Wait()
+			if not moveSuccess then
+				break
+			end
+			if fleeing == true then
+				if i == #waypoints then
+					wander()
+				end
+			end
+		end
+	else
+		getUnstuck()
 	end
 end
 
-function waitForChild(parent, childName)
-	local child = parent:findFirstChild(childName)
+function wander()
+	local randX = math.random(-50,50)
+	local randZ = math.random(-50,50)
+	local goal = myRoot.Position + Vector3.new(randX,0,randZ)
 
-	if child then
-		return child
+	pathToLocation(goal)
+end
+
+function findThreat()
+	local dist = 100
+	local threat = nil
+	for i,v in ipairs(workspace:GetChildren()) do
+		local human = v:FindFirstChild("Humanoid")
+		local torso = v:FindFirstChild("Torso") or v:FindFirstChild("HumanoidRootPart")
+		if human and torso and v ~= script.Parent then
+			if (myRoot.Position - torso.Position).Magnitude < dist then
+				threat = torso
+				dist = (myRoot.Position - torso.Position).Magnitude
+			end
+		end
+	end
+	return threat
+end
+
+function flee()
+	local threat = findThreat()
+	if threat then
+		local cframe = myRoot.CFrame * CFrame.new((threat.Position - myRoot.Position).Unit * 50)
+		spawn(function() pathToLocation(cframe.Position) end)
+	else
+		spawn(wander)
 	end
 
-	while true do
-		print(childName)
-
-		child = parent.ChildAdded:wait()
-
-		if child.Name==childName then
-			return child
+	fleeCountdown = fleeLength
+	if fleeing == false then
+		fleeing = true
+		myFace.Texture = "http://www.roblox.com/asset/?id=258192246"
+		myHuman.WalkSpeed = 25
+		repeat
+			wait(1)
+			fleeCountdown = fleeCountdown - 1 
+		until fleeCountdown <= 0 or myHuman.Health <= 0 
+		if dead == false then
+			myFace.Texture = "rbxasset://textures/face.png"
+			myHuman.WalkSpeed = 16 
 		end
+		fleeing = false
 	end
 end
 
-local Figure = script.Parent
-local Humanoid = waitForChild(Figure, "Humanoid")
-local Torso = waitForChild(Figure, "Torso")
-local Left = waitForChild(Figure, "Left Leg")
-local Right = waitForChild(Figure, "Right Leg")
+myHuman.Died:Connect(function()
+	myFace.Texture = "http://www.roblox.com/asset/?id=15426038"
+	dead = true 
+	wait(20)
+	clone.Parent = workspace
+	game:GetService("Debris"):AddItem(script.Parent,0.1)
+end)
 
-Humanoid.Jump = true
-
-Left.Touched:connect(onTouched)
-Right.Touched:connect(onTouched)
-
-while true do
-	wait(math.random(2, 6))
-
-	if CurrentPart ~= nil then
-		if math.random(1, 2) == 1 then
-			Humanoid.Jump = true
+myHuman.HealthChanged:Connect(function(health)
+	if dead == false then
+		if health < oldHealth then
+			flee()
 		end
+		oldHealth = health
+	end
+end)
 
-		Humanoid:MoveTo(Torso.Position + Vector3.new(math.random(-MaxInc, MaxInc), 0, math.random(-MaxInc, MaxInc)), CurrentPart)
+while wait() do
+	if myHuman.Health > 0 then
+		if fleeing == false then
+			wander()
+			wait(math.random(5,10))
+		end
+	else
+		break
 	end
 end
