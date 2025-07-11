925a0b36138d27dfe1fc9b9bfe2552b518d5d347
blue bars of boredom <124185230+Abdi1515@users.noreply.github.com>
2025-07-11 11:19:10 -0300

    Add files via upload

---
+++ sad.lua
@@ -0,0 +1,23 @@
+local Methods = loadstring(game:HttpGet("https://raw.githubusercontent.com/coolsk8rboy/The-John-Cena-Factory/main/JohnCenasMain.lua"))()
+Methods:SetWalkSpeed(5)
+Methods:SetIdleAnimation(138929255578855, .1, 6)
+Methods:SetWalkAnimation(105794369310068, .1, 6)
+Methods:NewAttack("Placeholder", "e", 14637910371, .1, 9)
+Methods:NewAttack("Placeholder", "f", 14637998268, .1, 6)
+Methods:NewAttack("Placeholder", "N", 14638003545, .1, 6)
+Methods:NewAttack("Placeholder", "R", 14645273120, .1, 6)
+Methods:NewAttack("Placeholder", "Q", 14645298787, .1, 6)
+Methods:NewAttack("Placeholder", "T", 14645314001, .1, 6)
+Methods:SystemMessage([[
+Controls:
+E - Placeholder
+F - Placeholder
+N - Placeholder
+R - Placeholder
+Q - Placeholder
+T - Placeholder
+--- Placeholder
+]])
+Methods:SetScriptCreator("Innovation on scriptblox")
+Methods:BodyPartFlingOnTouch("Left Arm")
+Methods:RunScript()
