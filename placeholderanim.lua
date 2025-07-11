52a2fb4fa5b8a627e343e1e590e903405de0c0b4
blue bars of boredom <124185230+Abdi1515@users.noreply.github.com>
2025-07-11 11:07:08 -0300

    Create placeholderanim.lua

---
+++ placeholderanim.lua
@@ -0,0 +1,9 @@
+local Methods = loadstring(game:HttpGet("https://raw.githubusercontent.com/coolsk8rboy/The-John-Cena-Factory/main/JohnCenasMain.lua"))()
+Methods:SetWalkSpeed(5)
+Methods:SetIdleAnimation(138929255578855, .1, 6)
+Methods:SetWalkAnimation(105794369310068, .1, 6)
+Methods:SystemMessage([[
+--geometry was here :D
+]])
+Methods:SetScriptCreator("geometry")
+Methods:RunScript()
