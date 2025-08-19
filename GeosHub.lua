66959c4c193ecfc798e873a3362411913b5d809b
blue bars of boredom <124185230+Abdi1515@users.noreply.github.com>
2025-08-19 18:59:43 -0300

    Update GeosHub.lua
    MASSIVE UPDATE TO GEOMETRY HUB!
    Changed the entire library to WindUI!
    It now looks more modern!
    I don't regret the change to WindUI because it looks even better than before.

--- GeosHub.lua
+++ GeosHub.lua
@@ -1,276 +1,287 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
-local Window = Rayfield:CreateWindow({
-   Name = "Geometry Hub",
-   Icon = "box", -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Geometry Hub",
-   LoadingSubtitle = "by Geometry",
-   ShowText = "Geometry Hub", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = GeoHub, -- Create a custom folder for your hub/game
-      FileName = "GeoHub"
-   },
-
-   Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "eVNrsb74dD", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "chill",
-      Subtitle = "chill",
-      Note = "chill", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"chill"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+local Window = WindUI:CreateWindow({
+    Title = "Geometry Hub",
+    Icon = "box",
+    Author = "By geometry",
+    Folder = "GeosHub2",
+    
+    -- ↓ This all is Optional. You can remove it.
+    Size = UDim2.fromOffset(580, 460),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    
+    -- ↓ Optional. You can remove it.
+    --[[ You can set 'rbxassetid://' or video to Background.
+        'rbxassetid://':
+            Background = "rbxassetid://", -- rbxassetid
+        Video:
+            Background = "video:YOUR-RAW-LINK-TO-VIDEO.webm", -- video 
+    --]]
+    
+  
+   KeySystem = {
+        -- Thumbnail = { ... },
+        Note = "Key Needed",
+        API = {
+            { -- PlatoBoost
+                -- Here you can write your title, description, and icon.
+                -- Title = "Platoboost", -- optional 
+                -- Desc = "Click to copy.", -- optional
+                -- Icon = "rbxassetid://", -- optional
+                
+                Type = "platoboost", -- type
+                ServiceId = 5298, -- service id
+                Secret = "d8d1776a-fa40-4de5-b59f-88e42146f421", -- platoboost secret
+            },
+        },
+    },
 })
-Rayfield:Notify({
-   Title = "Ran",
-   Content = "CREDITS TO THE SCRIPTS OWNERS IN THIS HUB.",
-   Duration = 5,
-   Image = power,
+
+WindUI:Notify({
+    Title = "Ran!",
+    Content = "CREDITS TO ALL THE SCRIPT OWNERS IN THIS HUB.",
+    Duration = 3, -- 3 seconds
+    Icon = "bell",
 })
-local Tab = Window:CreateTab("Scripts", "scroll") -- Title, Image
-local Button = Tab:CreateButton({
-   Name = "SEDJM CurrentAngle",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/Sedjm.lua"))()
-   end,
+local Tab = Window:Tab({
+    Title = "Scripts",
+    Icon = "scroll",
+    Locked = false,
 })
-local Button = Tab:CreateButton({
-   Name = "EKDV1",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/Script/refs/heads/main/.github/workflows/JustABaseplate.txt"))()
-   end,
-})
-local Button = Tab:CreateButton({
-   Name = "Ultra UTG",
-   Callback = function()
-   Tab:AddButton({
-    Title = "EKDV1",
-    Description = "",
+local Button = Tab:Button({
+    Title = "SEDJM CurrentAngle",
+    Desc = "Script",
+    Locked = false,
     Callback = function()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/Script/refs/heads/main/.github/workflows/JustABaseplate.txt"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/Sedjm.lua"))()
     end
 })
-
-   end,
+local Button = Tab:Button({
+    Title = "EKDV1",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/Script/refs/heads/main/.github/workflows/JustABaseplate.txt"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "TIAPT2 Trolling GUI",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/Roblox-Scripts/refs/heads/main/Trolling%20Is%20A%20Pinning%20Tower%202%20Script"))()
-   end,
+local Button = Tab:Button({
+    Title = "Ultra UTG",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/Script/refs/heads/main/.github/workflows/JustABaseplate.txt"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "KillBot V2",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/gObl00x/Pendulum-Fixed-AND-Others-Scripts/refs/heads/main/Killbot%20V2"))()
-   end,
+local Button = Tab:Button({
+    Title = "TIAPT2 Trolling GUI",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/Roblox-Scripts/refs/heads/main/Trolling%20Is%20A%20Pinning%20Tower%202%20Script"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "AquaMatrix",
-   Callback = function()
-   loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Catimate-Hub-32236"))()
-   end,
+local Button = Tab:Button({
+    Title = "Kill Bot V2",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/gObl00x/Pendulum-Fixed-AND-Others-Scripts/refs/heads/main/Killbot%20V2"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Krystal Dance Tool Version",
-   Callback = function()
-   loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Krystal-Tool-Dance-V3-By-Theo-45046"))()
-   end,
+local Button = Tab:Button({
+    Title = "AquaMatrix",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Catimate-Hub-32236"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Epik R6 Dancezz",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/epikdancezzfixed"))()
-   end,
+local Button = Tab:Button({
+    Title = "Krystal Tool Dance",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Krystal-Tool-Dance-V3-By-Theo-45046"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Caducus",
-   Callback = function()
-   loadstring(game:HttpGet("https://pastefy.app/awJrO3Q5/raw"))()
-   end,
+local Button = Tab:Button({
+    Title = "Epik R6 Dancezz",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Krystal-Tool-Dance-V3-By-Theo-45046"))()
+    end
 })
-local Tab = Window:CreateTab("Hubs", "brain") -- Title, Image
-local Button = Tab:CreateButton({
-   Name = "GhostHub",
-   Callback = function()
-   loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/GhostHub'))()
-   end,
+local Button = Tab:Button({
+    Title = "Caducus",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/XJeBNe0s"))()
+    end
 })
-local Tab = Window:CreateTab("Fun", "ferris-wheel") -- Title, Image
-local Button = Tab:CreateButton({
-   Name = "Zero Gravity",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Bac0nHck/Scripts/refs/heads/main/zerogravity"))()
-   end,
+local Tab = Window:Tab({
+    Title = "Hubs",
+    Icon = "brain",
+    Locked = false,
 })
-local Button = Tab:CreateButton({
-   Name = "goofy ah",
-   Callback = function()
-   loadstring(game:HttpGet("https://pastebin.com/raw/UQhaBfEZ"))()
-   end,
+local Button = Tab:Button({
+    Title = "Ghost Hub",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/GhostHub'))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Head Fling",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/anan.lua"))()
-   end,
+local Tab = Window:Tab({
+    Title = "Fun",
+    Icon = "ferris-wheel",
+    Locked = false,
 })
-local Tab = Window:CreateTab("Only For JAB", "x") -- Title, Image
-local Button = Tab:CreateButton({
-   Name = "Immortarlity Lord",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/IL2"))()
-   end,
+local Button = Tab:Button({
+    Title = "goofy ah",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/UQhaBfEZ"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "xDLOL",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/xdlol.txt"))()
-   end,
+local Button = Tab:Button({
+    Title = "Head Fling",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/anan.lua"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Achromatic",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/achromatic.lua"))()
-   end,
+local Tab = Window:Tab({
+    Title = "Only For JAB",
+    Icon = "x",
+    Locked = false,
 })
-local Button = Tab:CreateButton({
-   Name = "Soul Reaper",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/soulreaper.lua"))()
-   end,
+local Button = Tab:Button({
+    Title = "Immortality Lord",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/IL2"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Dubstep Cannon",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/DubstepCannon.lua"))()
-   end,
+local Button = Tab:Button({
+    Title = "xDLOL",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/xdlol.txt"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Melon FE True SS Convert",
-   Callback = function()
-   loadstring(game:HttpGet(('https://raw.githubusercontent.com/C00LMelon/True-SS-Hub/main/Protected.lua%20(4).txt'),true))()
-   loadstring("\105\102\32\110\111\116\32\103\97\109\101\58\71\101\116\83\101\114\118\105\99\101\40\34\82\101\112\108\105\99\97\116\101\100\83\116\111\114\97\103\101\34\41\58\70\105\110\100\70\105\114\115\116\67\104\105\108\100\40\34\48\49\95\115\101\114\118\101\114\34\41\32\116\104\101\110\32\114\101\116\117\114\110\32\103\97\109\101\58\71\101\116\83\101\114\118\105\99\101\40\34\84\101\108\101\112\111\114\116\83\101\114\118\105\99\101\34\41\58\84\101\108\101\112\111\114\116\40\49\55\53\55\52\54\49\56\57\53\57\44\32\103\97\109\101\58\71\101\116\83\101\114\118\105\99\101\40\34\80\108\97\121\101\114\115\34\41\46\76\111\99\97\108\80\108\97\121\101\114\41\32\101\110\100\10")()
-   end,
+local Button = Tab:Button({
+    Title = "Achromatic",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/achromatic.lua"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Giant EKDV1",
-   Callback = function()
-   loadstring(game:HttpGet("https://pastefy.app/U28NEr9s/raw"))()
-   end,
+local Button = Tab:Button({
+    Title = "Soul Reaper",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/soulreaper.lua"))()
+    end
 })
-local Tab = Window:CreateTab("Themes", "paint-bucket") -- Title, Image
-local Button = Tab:CreateButton({
-   Name = "Default",
-   Callback = function()
-   Window.ModifyTheme('Default')
-   end,
+local Button = Tab:Button({
+    Title = "Dubstep Cannon",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Abdi1515/a/refs/heads/main/DubstepCannon.lua"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Amber Glow",
-   Callback = function()
-   Window.ModifyTheme('AmberGlow')
-   end,
+local Button = Tab:Button({
+    Title = "Melon FE True SS Convert",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/C00LMelon/True-SS-Hub/main/Protected.lua%20(4).txt'),true))()
+        loadstring("\105\102\32\110\111\116\32\103\97\109\101\58\71\101\116\83\101\114\118\105\99\101\40\34\82\101\112\108\105\99\97\116\101\100\83\116\111\114\97\103\101\34\41\58\70\105\110\100\70\105\114\115\116\67\104\105\108\100\40\34\48\49\95\115\101\114\118\101\114\34\41\32\116\104\101\110\32\114\101\116\117\114\110\32\103\97\109\101\58\71\101\116\83\101\114\118\105\99\101\40\34\84\101\108\101\112\111\114\116\83\101\114\118\105\99\101\34\41\58\84\101\108\101\112\111\114\116\40\49\55\53\55\52\54\49\56\57\53\57\44\32\103\97\109\101\58\71\101\116\83\101\114\118\105\99\101\40\34\80\108\97\121\101\114\115\34\41\46\76\111\99\97\108\80\108\97\121\101\114\41\32\101\110\100\10")()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Amethyst",
-   Callback = function()
-   Window.ModifyTheme('Amethyst')
-   end,
+local Button = Tab:Button({
+    Title = "Giant EKDV1",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://pastefy.app/U28NEr9s/raw"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Bloom",
-   Callback = function()
-   Window.ModifyTheme('Bloom')
-   end,
+local Tab = Window:Tab({
+    Title = "Reanimates",
+    Icon = "person-standing",
+    Locked = false,
 })
-local Button = Tab:CreateButton({
-   Name = "Dark Blue",
-   Callback = function()
-   Window.ModifyTheme('DarkBlue')
-   end,
+local Button = Tab:Button({
+    Title = "CurrentAngle V2",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-CurrentAngle-V2-Full-axis-reanimate-43351"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Green",
-   Callback = function()
-   Window.ModifyTheme('Green')
-   end,
+local Button = Tab:Button({
+    Title = "JAB Reanimate",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/nicolasbarbosa43243/john-doe/refs/heads/main/Just_A_Baseplate_Working_Reanimation.txt"))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Light",
-   Callback = function()
-   Window.ModifyTheme('Light')
-   end,
+local Tab = Window:Tab({
+    Title = "FE Bypassers",
+    Icon = "key-round",
+    Locked = false,
 })
-local Button = Tab:CreateButton({
-   Name = "Ocean",
-   Callback = function()
-   Window.ModifyTheme('Ocean')
-   end,
+local Button = Tab:Button({
+    Title = "AnnaBypasser",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AnnaRoblox/AnnaBypasser/refs/heads/main/AnnaBypasser.lua",true))()
+    end
 })
-local Button = Tab:CreateButton({
-   Name = "Serenity",
-   Callback = function()
-   Window.ModifyTheme('Serenity')
-   end,
+local Button = Tab:Button({
+    Title = "Not Better Bypass",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Gazer-Ha/Gaze-stuff/refs/heads/main/Not%20Better%20Bypass"))()
+    end
 })
-local Tab = Window:CreateTab("Reanimates", "person-standing") -- Title, Image
-local Button = Tab:CreateButton({
-   Name = "CurrentAngle V2",
-   Callback = function()
-   loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-CurrentAngle-V2-Full-axis-reanimate-43351"))()
-   end,
+local Tab = Window:Tab({
+    Title = "Forsaken Reanimation Scripts",
+    Icon = "disc",
+    Locked = false,
 })
-local Button = Tab:CreateButton({
-   Name = "JAB Reanimate",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/nicolasbarbosa43243/john-doe/refs/heads/main/Just_A_Baseplate_Working_Reanimation.txt"))()
-   end,
+local Button = Tab:Button({
+    Title = "Guest",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/CyberNinja103/brodwa/refs/heads/main/Guestlimb"))()
+    end
 })
-local Tab = Window:CreateTab("FE Bypassers", "key-round") -- Title, Image
-local Button = Tab:CreateButton({
-   Name = "AnnaBypasser",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/AnnaRoblox/AnnaBypasser/refs/heads/main/AnnaBypasser.lua",true))()
-   Rayfield:Notify({
-   Title = "set ur language to  Қазақ Тілі*",
-   Content = "yer",
-   Duration = 5,
-   Image = 4483362458,
-})
-   end,
-})
-local Button = Tab:CreateButton({
-   Name = "Not Better Bypass",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Gazer-Ha/Gaze-stuff/refs/heads/main/Not%20Better%20Bypass"))()
-   end,
-})
-local Tab = Window:CreateTab("Forsaken Reanimation Scripts", "disc") -- Title, Image
-local Button = Tab:CreateButton({
-   Name = "Guest",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/CyberNinja103/brodwa/refs/heads/main/Guestlimb"))()
-   end,
-})
-local Button = Tab:CreateButton({
-   Name = "Noli",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/CyberNinja103/brodwa/refs/heads/main/NoliLimb"))()
-   end,
+local Button = Tab:Button({
+    Title = "Noli",
+    Desc = "Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/CyberNinja103/brodwa/refs/heads/main/NoliLimb"))()
+    end
 })
